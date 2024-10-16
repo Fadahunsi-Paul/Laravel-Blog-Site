eef5f31bdeaeb2341cf9b1f8824bb4d742e18281
unknown <scandiclamar@gmail.com>
2024-10-16 03:45:16 +0000

    laravel

--- app/Http/Controllers/userController.php
+++ app/Http/Controllers/userController.php
@@ -2,9 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class userController extends Controller
-{
-    //
+{   
+    public function login(Request $request){
+        $loginfields=$request->validate([
+            'email'=>['required'],
+            'password'=>['required']
+        ]);
+        return 'login successful';
+    }
+
+
+    public function register(Request $request){
+        $fields = $request->validate(
+            [
+            'name'=> ["required",'min:2','max:15', Rule::unique('users','name')],
+            'email' => ["required","email",Rule::unique('users','email')],
+            "password" => ["required",'min:3','max:15']
+            ]
+        );
+        $fields['password']=bcrypt($fields['password']);
+        $user=User::create($fields);
+        auth()->login($user);
+        return redirect('/');
+    }
 }
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,13 +5,27 @@
     <title>homepage</title>
 </head>
 <body>
+    @auth
+    <div style="border:2px solid black;">
+        <h2>Login</h2>
+        <form action="/register" method='POST'>
+            @csrf
+            <input name="email" type="text" placeholder="Email">
+            <input name="password" type="text" placeholder="Password">
+            <button>Login</button>
+        </form>
+    </div>
+    @else 
+
+    @endauth
+
     <div style="border:2px solid black;">
         <h2>Register</h2>
         <form action="/register" method='POST'>
             @csrf
-            <input type="text" placeholder="Name">
-            <input type="text" placeholder="Email">
-            <input type="text" placeholder="Password">
+            <input name="name" type="text" placeholder="Name">
+            <input name="email" type="text" placeholder="Email">
+            <input name="password" type="text" placeholder="Password">
             <button>register</button>
         </form>
     </div>
